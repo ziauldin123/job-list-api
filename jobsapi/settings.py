4623a63ae14759475832e5c2cd8ffbfb95dcb5bf
ziauldin123 <mziauldin2@gmail.com>
2020-07-03 12:16:44 +0500

    setting Cors

--- jobsapi/settings.py
+++ jobsapi/settings.py
@@ -82,11 +82,11 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
